bb4030c54a89d53f1a7a28f6a90cea27f1a9c905
zoubinthw <316324334@qq.com>
2024-05-05 23:15:01 +0800

    14 db user authentication

---
+++ src/main/java/com/atguigu/securitydemo/config/DBUserDetailsManager.java
@@ -0,0 +1,79 @@
+package com.atguigu.securitydemo.config;
+
+import com.atguigu.securitydemo.entity.User;
+import com.atguigu.securitydemo.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class DBUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+
+    /**
+     * 通过用户名从数据库中获取用户信息
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        User user = userMapper.selectOne(queryWrapper);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        } else {
+            // 用户权限(角色)列表
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getEnabled(),
+                    true,  //用户账号是否过期
+                    true, // 用户凭证是否过期
+                    true, // 用户是否未被锁定
+                    authorities // 权限列表
+            );
+        }
+    }
+}
--- src/main/java/com/atguigu/securitydemo/config/WebSecurityConfig.java
+++ src/main/java/com/atguigu/securitydemo/config/WebSecurityConfig.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration // 配置类
 //@EnableWebSecurity // 开启spring security自定义配置(springboot项目可省略)
 public class WebSecurityConfig {
-    @Bean
-    public UserDetailsService userDetailsService() {
-        // 创建基于内存的用户信息管理器
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//    @Bean
+//    public UserDetailsService userDetailsService() { // 基于内存的用户信息管理器
+//        // 创建基于内存的用户信息管理器
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//
+//        // 使用InMemoryUserDetailsManager来管理我们的UserDetails类型对象
+//        manager.createUser(
+//                // 创建UserDetails类型对象, 用于管理用户名和密码, 角色, 权限等内容
+//                User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build()
+//        );
+//        return manager;
+//    }
 
-        // 使用InMemoryUserDetailsManager来管理我们的UserDetails类型对象
-        manager.createUser(
-                // 创建UserDetails类型对象, 用于管理用户名和密码, 角色, 权限等内容
-                User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build()
-        );
-        return manager;
-    }
+//    @Bean // 还有种方法来配置这个Bean, 因为我们只返回manager, 可以直接在DBUserDetailsManager这个类上添加@Component注解即可
+//    public UserDetailsService userDBDetailsService() { // 基于数据库的用户信息管理器
+//        // 创建基于数据库的用户信息管理器
+//        return new DBUserDetailsManager();
+//    }
 }
