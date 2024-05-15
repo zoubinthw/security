816024ffb3c97eac53329f9be814b9fcc7020141
zoubinthw <316324334@qq.com>
2024-05-15 23:54:24 +0800

    [29] user authentication.

--- src/main/java/com/atguigu/securitydemo/controller/IndexController.java
+++ src/main/java/com/atguigu/securitydemo/controller/IndexController.java
@@ -1,12 +1,31 @@
 package com.atguigu.securitydemo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class IndexController {
     @GetMapping("/")
-    public String hello() {
-        return "index";
+    public Map<Object, Object> hello() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        Object principal = authentication.getPrincipal();
+        Object credentials = authentication.getCredentials(); // 脱敏
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String name = authentication.getName();
+        Map<Object, Object> result = new HashMap<>();
+        result.put("username", name);
+        result.put("authorities", authorities);
+
+        return result;
     }
 }
